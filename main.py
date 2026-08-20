5f417f1ab0669b148b967d93407b23d6e3142294
yastrab1 <lipka.luki@gmail.com>
2026-08-20 13:46:52 +0200

    button class

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import pygame
 import time
 
+class Button:
+    def __init__(self,rect:pygame.Rect, onClick) -> None:
+        self.rect = rect
+        self.onClick = onClick
+
+    def render(self,win:pygame.Surface):
+        pygame.draw.rect(win, "gray", self.rect)
+
+    def consumeEvent(self,event:pygame.event.Event):
+        if event.type == pygame.MOUSEBUTTONUP:
+            x,y = pygame.mouse.get_pos()
+            if self.rect.collidepoint(x,y):
+                self.onClick()
+
+
+def yayy():
+    print("yay")
+
 pygame.init()
 win = pygame.display.set_mode((1920,1200))
 font = pygame.font.Font("LobsterTwo-Regular.ttf",100)
 running = True
 rect = pygame.Rect(((50,50),(150,40)))
+tlacitka:list[Button] = []
+
+tlacitko = Button(rect,yayy)
+tlacitka.append(tlacitko)
 peniazteky = 0
 
 while running:
@@ -14,13 +36,13 @@ while running:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running= False
-        if e.type == pygame.MOUSEBUTTONUP:
-            x,y = pygame.mouse.get_pos()
-            if rect.collidepoint(x,y):
-                peniazteky += 1
+        for t in tlacitka:
+            t.consumeEvent(e)
+
 
     peniazteky_surf = font.render(str(peniazteky),True,'orange')
     win.blit(peniazteky_surf,rect.topright)
-    pygame.draw.rect(win, "gray", rect)
+    
 
     pygame.display.flip()
+
